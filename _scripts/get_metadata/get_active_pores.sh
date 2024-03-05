668aa2ef5e1b39b3bd8ac98726dcceaacc54b67a
Daria <daria-meyer@web.de>
2024-03-05 16:45:34 +0100

    Create get_active_pores.sh

---
+++ _scripts/get_metadata/get_active_pores.sh
@@ -0,0 +1,7 @@
+## Extract the number of active pores at run start
+
+## old minKNOW version -- mux_scan_data_<fc_id>_<run_id>.csv
+head -n 2049 mux_scan_data_<fc_id>_<run_id>.csv | awk -F ',' -v col='mux_scan_assessment' 'NR==1{for(i=1;i<=NF;i++){if($i==col){c=i;break}} print $c} NR>1{print $c}' | grep 'single_pore\|other' | wc -l
+
+## new minKNOW version
+head -n 2049 pore_scan_data_<fc_id>_<run_id>_<run_id>.csv | awk -F ',' -v col='mux_scan_assessment' 'NR==1{for(i=1;i<=NF;i++){if($i==col){c=i;break}} print $c} NR>1{print $c}' | grep 'single_pore\|other' | wc -l
