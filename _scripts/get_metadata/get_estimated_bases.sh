0ec6edf59abece820ab3a21a7ca33310a01bb1ef
Daria <daria-meyer@web.de>
2024-03-05 14:53:34 +0100

    Update get_estimated_bases.sh

--- _scripts/get_metadata/get_estimated_bases.sh
+++ _scripts/get_metadata/get_estimated_bases.sh
@@ -1,7 +1,7 @@
 ## How to get estimated number of sequenced bases from the different potential output files
 
-## Old & new MinKNOW version:
+## old & new MinKNOW version:
 
-## From the troughtput file haven the following syntax: throughput_<FC>_<run>.csv
+# From the troughtput file haven the following syntax: throughput_<FC>_<run>.csv
 awk -F ',' -v col='Estimated Bases' 'NR==1{for(i=1;i<=NF;i++){if($i==col){c=i;break}} print $c} NR>1{print $c}' throughput_<FC>_<run>.csv | tail -n1
 
